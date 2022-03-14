67dce6501fbb3fd03def6b152ebbf42df99fa666
June Skálletty <101154164+juneskalletty@users.noreply.github.com>
2022-03-14 17:26:30 -0400

    commit inicial

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
+
+listaSelecaoPokemons.forEach(pokemon => {
+    pokemon.addEventListener('click', () => {
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        cartaoPokemonAberto.classList.remove('aberto')
+
+        const idPokemonSelecionado = pokemon.attributes.id.value
+
+        const cartaoPkemonParaAbrir = document.getElementById('cartao-' + idPokemonSelecionado)
+        cartaoPkemonParaAbrir.classList.add('aberto')
+
+        const pokemonAtivoNaListagem = document.querySelector('.ativo')
+        pokemonAtivoNaListagem.classList.remove('ativo')
+
+        const pokemonSelecionadoNalistagem = document.getElementById(idPokemonSelecionado)
+        pokemonSelecionadoNalistagem.classList.add('ativo')
+
+    })
+})
